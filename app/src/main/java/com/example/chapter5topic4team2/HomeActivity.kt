5fc5b87e80fc2188f29fe9ffb1f5b0fd42fc9aa2
RahmadinaIkaP <rahmadina.ika@student.umn.ac.id>
2022-10-04 14:30:54 +0700

    add reyclerview + adapter, and get data film (half)

--- app/src/main/java/com/example/chapter5topic4team2/HomeActivity.kt
+++ app/src/main/java/com/example/chapter5topic4team2/HomeActivity.kt
@@ -2,16 +2,42 @@ package com.example.chapter5topic4team2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chapter5topic4team2.adapter.FilmAdapter
 import com.example.chapter5topic4team2.databinding.ActivityHomeBinding
+import com.example.chapter5topic4team2.model.FilmResponseItem
+import com.example.chapter5topic4team2.viewmodel.FilmViewModel
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), FilmAdapter.FilmInterface {
     private lateinit var binding : ActivityHomeBinding
+
+    private lateinit var adapter: FilmAdapter
+    private val filmViewModel : FilmViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setDataRv()
+    }
+
+    private fun setDataRv() {
+        adapter = FilmAdapter(this)
+
+        binding.apply {
+            filmViewModel.showFilmList()
+            filmViewModel.getLiveDataFilms().observe(this@HomeActivity){
+                adapter.setData(it)
+            }
+
+            rvFilm.adapter = adapter
+            rvFilm.layoutManager = LinearLayoutManager(this@HomeActivity, LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
+    override fun onItemClick(film: FilmResponseItem) {
 
     }
 }
---
+++ app/src/main/java/com/example/chapter5topic4team2/adapter/FilmAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.chapter5topic4team2.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chapter5topic4team2.databinding.ItemFilmBinding
+import com.example.chapter5topic4team2.model.FilmResponseItem
+
+class FilmAdapter(val onClickListener : FilmInterface) : RecyclerView.Adapter<FilmAdapter.ViewHolder>() {
+
+    private var diffCallback = object : DiffUtil.ItemCallback<FilmResponseItem>(){
+        override fun areItemsTheSame(oldItem: FilmResponseItem, newItem: FilmResponseItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: FilmResponseItem, newItem: FilmResponseItem): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    inner class ViewHolder(private val binding: ItemFilmBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(film : FilmResponseItem){
+                binding.apply {
+                    dataFilm = film
+                    itemView.setOnClickListener {
+                        onClickListener.onItemClick(film)
+                    }
+                }
+            }
+    }
+
+    interface FilmInterface {
+        fun onItemClick(film: FilmResponseItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = ItemFilmBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val film = differ.currentList[position]
+        holder.bind(film)
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    fun setData(data : List<FilmResponseItem>){
+        differ.submitList(data)
+    }
+
+}
